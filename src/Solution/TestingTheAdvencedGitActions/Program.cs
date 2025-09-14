45340d791eadf22eafdf0472fcb32d175179196f
- <your-github-email@example.com>
2025-09-14 17:14:13 +0500

    fsdfsdfsd

--- src/Solution/TestingTheAdvencedGitActions/Program.cs
+++ src/Solution/TestingTheAdvencedGitActions/Program.cs
@@ -12,7 +12,6 @@
             Console.WriteLine("method");
             Console.WriteLine("method");
             Console.WriteLine("method");
-            Console.WriteLine("method");
         }
     }
 }
