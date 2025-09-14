8d9be892f1c1011250d4e11529933c56fa940ab1
- <siddiqjonovsaidabror@gmail.com>
2025-09-14 18:24:41 +0500

    new squashed commit

--- src/Solution/TestingTheAdvencedGitActions/Class1.cs
+++ src/Solution/TestingTheAdvencedGitActions/Class1.cs
@@ -8,5 +8,12 @@ namespace TestingTheAdvencedGitActions
 {
     internal class Class1
     {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2{ get; set; }
+        public int MyProperty3 { get; set; }
+        public int MyProperty5 { get; set; }
+        public int MyProperty4 { get; set; }
+        public int MyProperty6 { get; set; }
     }
 }
